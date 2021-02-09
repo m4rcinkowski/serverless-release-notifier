90753ebc1a10c68d145c296d78afd7a8a9390bef
Tomek Marcinkowski <tomasz.marcinkowski@rst.com.pl>
2021-02-09 20:03:32 +0100

    handler code reformat

--- handler.ts
+++ handler.ts
@@ -1,13 +1,13 @@
 import { DynamoDBStreamHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import {Converter} from 'aws-sdk/clients/dynamodb';
+import { Converter } from 'aws-sdk/clients/dynamodb';
 
 export const onNewRelease: DynamoDBStreamHandler = async (event, _context) => {
-  console.debug(`Received ${event.Records.length} event(s)`);
+    console.debug(`Received ${event.Records.length} event(s)`);
 
-  event.Records.filter((record) => record.eventName === 'INSERT').forEach((record) => {
-    const attributes = Converter.unmarshall(record.dynamodb.NewImage);
+    event.Records.filter((record) => record.eventName === 'INSERT').forEach((record) => {
+        const attributes = Converter.unmarshall(record.dynamodb.NewImage);
 
-    console.log('Processing new table item', attributes);
-  });
+        console.log('Processing new table item', attributes);
+    });
 };
